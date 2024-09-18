8408ec21a367a0c66b2de5940da5e26aae8ba48a
ALS_02 <ALS_02@ALS_02>
2024-09-18 11:08:05 +0700

    Edit 2024-09-18 10_53 Add icon

--- PdfCombineApp/frmMain.cs
+++ PdfCombineApp/frmMain.cs
@@ -3,9 +3,9 @@ using PdfSharpCore.Pdf;
 
 namespace PdfCombineApp
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
